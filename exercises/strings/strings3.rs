ab28f4114ae7ebf98565cc917e40c3b1c1567a0c
qcloud <ubuntu@localhost.localdomain>
2023-10-19 21:05:57 +0800

    day3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,17 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+   String::from(input)
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.to_string()
 }
 
 #[cfg(test)]
